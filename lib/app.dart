d1e89a91f659d9ab80cf743a8890121486b4c7d7
Wassim Blilita <wassim.blilita19@gmail.com>
2026-02-26 14:09:47 +0100

    ..

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mon App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1A9C98)),
